488c17a15e8df00b8f64a3aa9c10040ca029d36b
ssy80 <soonyee80@gmail.com>
2023-08-01 17:13:45 +0800

    commit modify Nav

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import './App.css';
-import Header from "./views/Header.js"
+import './css/App.css';
 import Main from "./views/Main.js"
 import Nav from "./views/Nav.js"
 import Footer from "./views/Footer.js"
+import BookingPage from "./views/BookingPage";
+import BookingConfirmation from "./views/BookingConfirmation";
 
+import { Routes, Route, Link } from "react-router-dom";
 
 
 
 function App() {
   return (
-    <>
-    <Header></Header>
+
+    <div className="main-flex">
+    
     <Nav></Nav>
-    <Main></Main>
+  
+   <Routes> 
+    <Route path="/" element={<Main/>} ></Route>
+    <Route path="home" element={<Main/>} ></Route>
+    <Route path="/booking" element={<BookingPage />} ></Route>
+    <Route path="/bookingConfirmation" element={<BookingConfirmation />} ></Route>
+    </Routes>
+
     <Footer></Footer>
-    </>
+
+ 
+    </div>
+   
+   
+
+
+   
+
+
+
   );
 }
 
